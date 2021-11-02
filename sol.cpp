ac170dd3cc11c8d264e050d806b686868bae3342
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-11-02 16:00:18 +0800

    Add files via upload

---
+++ sol.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		long long n;
+		cin >> n;
+		// if 'n' is less than or equal to 2, then the answer will always be equal to 1
+		long long ans = 1;
+		if (n > 2) {
+			// otherwise, we need to use the formula for finding sum of first 'n - 2' multiples
+			// of 2 ((n - 2) was used because {1, 0} should be first 2 elements of the array)
+			ans += (2 * ((n - 2) * (n - 2 + 1) / 2));
+		}
+		cout << ans << '\n';
+	}
+	return 0;
+}
